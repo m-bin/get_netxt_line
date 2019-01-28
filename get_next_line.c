40fcee3c46ccda2f24ac45e702acaacc183e9ffe
Marguerite BIN <mbin@e1r3p3.42.fr>
2019-01-28 19:48:09 +0100

    presque tout marche

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mbin <mbin@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/26 17:54:46 by mbin              #+#    #+#             */
-/*   Updated: 2019/01/26 18:18:11 by mbin             ###   ########.fr       */
+/*   Updated: 2019/01/28 19:28:27 by mbin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ char	*ft_clean(char **file, int n)
 {
 	char	*fresh;
 
+	if ((*file)[n + 1] == 0)
+	{
+		ft_strdel(file);
+		return (NULL);
+	}
 	fresh = ft_strdup(*file + n + 1);
 	ft_strdel(file);
 	return (fresh);
@@ -26,15 +31,16 @@ int	read_file(const int fd, char **file)
 	char	buf[BUFF_SIZE + 1];
 	int		r;
 
-	if (fd < 0)
+	if (fd == -1)
 		return (-1);
+	ft_putchar('a');
 	while ((r = read(fd, buf, BUFF_SIZE)))
 	{
+		ft_putchar('b');
 		buf[r] = 0;
 		*file = ft_strjoinfree(*file, buf);
-		ft_putchar('a');
-		if (*file == NULL)
-			return (-1);
+		if (!file || !(*file))
+			return (-2);
 		if (r == -1)
 		{
 			ft_strdel(file);
@@ -48,25 +54,31 @@ int	read_file(const int fd, char **file)
 
 int	write_line(char **file, char **line)
 {
-	int	i;
+	int		i;
+	char	*ligne;
+	char	*cpfile;
 
+	cpfile = *file;
 	i = 0;
-	while ((*file)[i] && (*file)[i] != '\n')
+	while (cpfile[i] && cpfile[i] != '\n')
 		i++;
-	(*line) = (char *)malloc((i + 1) * sizeof(char));
-	if (*line == NULL)
+	ligne = (char *)malloc((i + 1) * sizeof(char));
+	if (!ligne)
+		return (-1);
+	if (ligne == NULL)
 		return (-1);
 	i = 0;
-	while ((*file)[i] && (*file)[i] != '\n')
+	while (cpfile[i] && cpfile[i] != '\n')
 	{
-		(*line)[i] = (*file)[i];
+		ligne[i] = cpfile[i];
 		i++;
 	}
-	(*line)[i] = 0;
-	if ((*file)[i])
+	ligne[i] = 0;
+	if (cpfile[i] == '\n')
 		*file = ft_clean(file, i);
 	else
 		ft_strdel(file);
+	*line = ligne;
 	return (1);
 }
 
@@ -77,11 +89,22 @@ int	get_next_line(const int fd, char **line)
 
 	r = read_file(fd, &file);
 	if (r == -1)
+	{
+		ft_strdel(&file);
 		return (-1);
+	}
 	if (r == 0)
 		return (0);
 	r = write_line(&file, line);
 	if (r == -1)
+	{
+		ft_strdel(&file);
 		return (-1);
+	}
+	if (r == 0)
+	{
+		ft_strdel(&file);
+		return (0);
+	}
 	return (1);
 }
