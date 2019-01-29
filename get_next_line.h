f879766cf79fe59211ba225609c35968b6f18c4a
Marguerite BIN <mbin@e1r9p12.42.fr>
2019-01-29 22:03:17 +0100

    ca marche maintenant

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: mbin <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/23 19:49:46 by mbin              #+#    #+#             */
-/*   Updated: 2019/01/26 18:16:06 by mbin             ###   ########.fr       */
+/*   Updated: 2019/01/29 21:58:57 by mbin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 50
+# define BUFF_SIZE 1000
 #include "libft/libft.h"
 #include <fcntl.h>
 
