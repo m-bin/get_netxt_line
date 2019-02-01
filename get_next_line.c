927293a1a503380b03fd23eec99f092db279f92a
Marguerite BIN <mbin@e2r5p9.42.fr>
2019-02-01 21:59:15 +0100

    final?

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mbin <mbin@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/26 17:54:46 by mbin              #+#    #+#             */
-/*   Updated: 2019/01/29 22:09:50 by cmace            ###   ########.fr       */
+/*   Updated: 2019/01/31 18:07:41 by mbin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,12 @@ int		read_file(const int fd, char **file)
 {
 	char	buf[BUFF_SIZE + 1];
 	int		r;
+	char	*is_there_n;
 
 	if (fd == -1)
 		return (-1);
-	while ((r = read(fd, buf, BUFF_SIZE)))
+	is_there_n = NULL;
+	while ((r = read(fd, buf, BUFF_SIZE)) && !is_there_n)
 	{
 		buf[r] = 0;
 		*file = ft_strjoinfree(*file, buf);
@@ -44,6 +46,7 @@ int		read_file(const int fd, char **file)
 			ft_strdel(file);
 			return (-1);
 		}
+		is_there_n = ft_strchr(*file, '\n');
 	}
 	if (*file == NULL)
 		return (0);
@@ -60,7 +63,7 @@ int		write_line(char **file, char **line)
 	i = 0;
 	while (cpfile[i] && cpfile[i] != '\n')
 		i++;
-	if ((ligne = (char *)malloc((i + 1) * sizeof(char))))
+	if (!(ligne = (char *)malloc((i + 1) * sizeof(char))))
 		return (-1);
 	if (ligne == NULL)
 		return (-1);
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mbin <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/23 19:49:46 by mbin              #+#    #+#             */
-/*   Updated: 2019/01/29 21:58:57 by mbin             ###   ########.fr       */
+/*   Updated: 2019/01/31 18:41:35 by mbin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 # define GET_NEXT_LINE_H
 
 # define BUFF_SIZE 1000
-#include "libft/libft.h"
-#include <fcntl.h>
+# include "libft/libft.h"
+# include <fcntl.h>
 
-int		get_next_line(const int fd, char **line);
+int				get_next_line(const int fd, char **line);
 
 #endif
